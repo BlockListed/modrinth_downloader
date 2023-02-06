b501dad9ce48cbd647b0f4be8e2fe21781957493
BlockListed <44610569+BlockListed@users.noreply.github.com>
2023-02-06 21:51:18 +0100

    replace weird copying stuff with std implementation

--- src/download/hash.rs
+++ src/download/hash.rs
@@ -1,4 +1,4 @@
-use std::io::{Result, Read};
+use std::io::{Result, copy};
 use std::fs::File;
 use std::time::Instant;
 use std::path;
@@ -18,16 +18,7 @@ pub fn hash_file(path: &path::Path) -> Result<String> {
 
     let mut sha = Sha512::new();
     // Creating a fixed sized buffer to reduce memory usage.
-    let mut buf: Vec<u8> = vec![0u8; 2usize.pow(2*10)];
-    loop {
-        let bytes_read = file.read(&mut buf)?;
-        if bytes_read == 0 {
-            log::trace!("Fully read file, {}!", path.display());
-            break
-        }
-        log::trace!("Read {} bytes from file, {}¡", bytes_read, path.display());
-        sha.update(&buf[0..bytes_read]);
-    }
+    copy(&mut file, &mut sha)?;
     let hash = sha.finalize();
 
     log::debug!("Took {} millis to hash!", start.elapsed().as_millis());
